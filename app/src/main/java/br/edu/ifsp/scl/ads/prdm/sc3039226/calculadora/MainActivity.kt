11703cd9bac4aadb5cba649d48cf1c891066bcd3
DylanPetrucelli <dtpetrucelli@gmail.com>
2025-08-29 16:33:22 -0300

    feat: Create add number feature

--- app/src/main/java/br/edu/ifsp/scl/ads/prdm/sc3039226/calculadora/MainActivity.kt
+++ app/src/main/java/br/edu/ifsp/scl/ads/prdm/sc3039226/calculadora/MainActivity.kt
@@ -2,15 +2,20 @@ package br.edu.ifsp.scl.ads.prdm.sc3039226.calculadora
 
 import android.annotation.SuppressLint
 import android.os.Bundle
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import br.edu.ifsp.scl.ads.prdm.sc3039226.calculadora.databinding.ActivityMainBinding
+import kotlin.properties.Delegates
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private var currOp: Int = 0
+    //This number represents the current operation being used
+    //zero is nothing
 
     @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -41,18 +46,22 @@ class MainActivity : AppCompatActivity() {
         binding.plus.setOnClickListener {
             binding.smallNum.text = binding.bigNum.text.toString() + " + "
             binding.bigNum.text = ""
+            currOp = 1
         }
         binding.minus.setOnClickListener {
             binding.smallNum.text = binding.bigNum.text.toString() + " - "
             binding.bigNum.text = ""
+            currOp = 2
         }
         binding.multiply.setOnClickListener {
             binding.smallNum.text = binding.bigNum.text.toString() + " * "
             binding.bigNum.text = ""
+            currOp = 3
         }
         binding.divide.setOnClickListener {
             binding.smallNum.text = binding.bigNum.text.toString() + " / "
             binding.bigNum.text = ""
+            currOp = 4
         }
 
         binding.Clear.setOnClickListener {
@@ -67,7 +76,9 @@ class MainActivity : AppCompatActivity() {
         }
 
         binding.equals.setOnClickListener {
-            //nothing here yet im just commiting so i can save
+            when (currOp) {
+                1 -> addNumbers()
+            }
         }
     }
 
@@ -76,4 +87,15 @@ class MainActivity : AppCompatActivity() {
             binding.bigNum.text = ""
         }
     }
+
+    private fun addNumbers () {
+        val firstNumber = binding.smallNum.text.toString().trim()
+        val secNumber = binding.bigNum.text.toString().trim()
+
+        val num1 = firstNumber.substring(0, firstNumber.length -2).toDouble()
+        val num2 = secNumber.toDouble()
+
+        binding.bigNum.text = (num1 + num2).toString()
+    }
+
 }
