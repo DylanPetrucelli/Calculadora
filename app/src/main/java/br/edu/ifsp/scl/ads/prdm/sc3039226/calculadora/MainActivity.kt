445fd90c183fa6910a18cf4a3da70412380bcad3
DylanPetrucelli <dtpetrucelli@gmail.com>
2025-08-29 14:39:12 -0300

    feat: add onClickListener to number buttons

--- app/src/main/java/br/edu/ifsp/scl/ads/prdm/sc3039226/calculadora/MainActivity.kt
+++ app/src/main/java/br/edu/ifsp/scl/ads/prdm/sc3039226/calculadora/MainActivity.kt
@@ -5,16 +5,37 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import br.edu.ifsp.scl.ads.prdm.sc3039226.calculadora.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        ViewCompat.setOnApplyWindowInsetsListener(binding.main) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+    }
+}
+
+        binding.one.setOnClickListener { binding.bigNum.append("1") }
+        binding.two.setOnClickListener { binding.bigNum.append("2") }
+        binding.three.setOnClickListener { binding.bigNum.append("3") }
+        binding.four.setOnClickListener { binding.bigNum.append("4") }
+        binding.five.setOnClickListener { binding.bigNum.append("5") }
+        binding.six.setOnClickListener { binding.bigNum.append("6") }
+        binding.seven.setOnClickListener { binding.bigNum.append("7") }
+        binding.eight.setOnClickListener { binding.bigNum.append("8") }
+        binding.nine.setOnClickListener { binding.bigNum.append("9") }
+        binding.zero.setOnClickListener { binding.bigNum.append("0") }
+        binding.dot.setOnClickListener { binding.bigNum.append(".") }
+
     }
 }
